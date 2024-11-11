665710b1f95a2b0b462da17f461d6e25877d244e
Tài Lê <taic21a.th1@gmail.com>
2024-11-11 19:34:39 +0700

    ok

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const path = require("path");
+
+const PORT = 11121;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/f/:slug", (req, res) => {
+  const slug = req.params.slug;
+  return res.render("home", { slug });
+});
+
+app.get("/f/:page/:slug", (req, res) => {
+  const slug = req.params.slug;
+  const page = req.params.page;
+  if (!page) {
+    return res.render("home", { slug });
+  } else {
+    switch (page) {
+      case "yoga":
+        return res.render("yoga", { slug });
+      default:
+        return res.render("home", { slug });
+    }
+  }
+});
+
+app.listen(PORT, () => console.log(`App is running with port ${PORT}`));
